3ee96ddd6a8527db43e0b9568797e08b50ecad54
Jethro Estrada <jethroestrada237@gmail.com>
2020-07-09 05:31:14 +0800

    Update on docs

--- src/routes/v1/contact.route.js
+++ src/routes/v1/contact.route.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const auth = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
 const contactValidation = require('../../validations/contact.validation');
 const contactController = require('../../controllers/contact.controller');
@@ -26,10 +25,7 @@ module.exports = router;
  *  /contacts:
  *    post:
  *      summary: Create a contact
- *      description: Only admins can create other contacts.
  *      tags: [Contacts]
- *      security:
- *        - bearerAuth: []
  *      requestBody:
  *        required: true
  *        content:
@@ -37,30 +33,29 @@ module.exports = router;
  *            schema:
  *              type: object
  *              required:
- *                - name
+ *                - firstName
+ *                - lastName
+ *                - companyName
  *                - email
- *                - password
- *                - role
+ *                - phoneNumber
  *              properties:
- *                name:
+ *                firstName:
+ *                  type: string
+ *                lastName:
+ *                  type: string
+ *                companyName:
  *                  type: string
  *                email:
  *                  type: string
  *                  format: email
- *                  description: must be unique
- *                password:
- *                  type: string
- *                  format: password
- *                  minLength: 8
- *                  description: At least one number and one letter
- *                role:
- *                   type: string
- *                   enum: [contact, admin]
+ *                phoneNumber:
+ *                   type: number
  *              example:
- *                name: fake name
- *                email: fake@example.com
- *                password: password1
- *                role: contact
+ *                firstName: Jethro
+ *                lastName: Estrada
+ *                companyName: Amazon
+ *                email: jethroestrada237@gmail.com
+ *                phoneNumber: 639159897270
  *      responses:
  *        "201":
  *          description: Created
@@ -68,75 +63,17 @@ module.exports = router;
  *            application/json:
  *              schema:
  *                 $ref: '#/components/schemas/Contact'
- *        "400":
- *          $ref: '#/components/responses/DuplicateEmail'
- *        "401":
- *          $ref: '#/components/responses/Unauthorized'
- *        "403":
- *          $ref: '#/components/responses/Forbidden'
  *
  *    get:
  *      summary: Get all contacts
- *      description: Only admins can retrieve all contacts.
  *      tags: [Contacts]
- *      security:
- *        - bearerAuth: []
- *      parameters:
- *        - in: query
- *          name: name
- *          schema:
- *            type: string
- *          description: Contact name
- *        - in: query
- *          name: role
- *          schema:
- *            type: string
- *          description: Contact role
- *        - in: query
- *          name: sortBy
- *          schema:
- *            type: string
- *          description: sort by query in the form of field:desc/asc (ex. name:asc)
- *        - in: query
- *          name: limit
- *          schema:
- *            type: integer
- *            minimum: 1
- *          default: 10
- *          description: Maximum number of contacts
- *        - in: query
- *          name: page
- *          schema:
- *            type: integer
- *            minimum: 1
- *            default: 1
- *          description: Page number
  *      responses:
  *        "200":
  *          description: OK
  *          content:
  *            application/json:
  *              schema:
- *                type: object
- *                properties:
- *                  results:
- *                    type: array
- *                    items:
- *                      $ref: '#/components/schemas/Contact'
- *                  page:
- *                    type: integer
- *                    example: 1
- *                  limit:
- *                    type: integer
- *                    example: 10
- *                  totalPages:
- *                    type: integer
- *                    example: 1
- *                  totalResults:
- *                    type: integer
- *                    example: 1
- *        "401":
- *          $ref: '#/components/responses/Unauthorized'
- *        "403":
- *          $ref: '#/components/responses/Forbidden'
+ *                type: array
+ *                items:
+ *                  $ref: '#/components/schemas/Contact'
  */
