01ba4804e9c3e6ec0014ebe6b41f74ae378c6bc8
Jethro Estrada <jethroestrada237@gmail.com>
2020-07-09 03:56:15 +0800

    Removed pagination

--- src/models/contact.model.js
+++ src/models/contact.model.js
@@ -47,35 +47,6 @@ const contactSchema = mongoose.Schema(
 contactSchema.plugin(toJSON);
 
 /**
- * Check if email is taken
- * @param {string} email - The contact's email
- * @param {ObjectId} [excludeContactId] - The id of the contact to be excluded
- * @returns {Promise<boolean>}
- */
-contactSchema.statics.isEmailTaken = async function (email, excludeContactId) {
-  const contact = await this.findOne({ email, _id: { $ne: excludeContactId } });
-  return !!contact;
-};
-
-/**
- * Check if password matches the contact's password
- * @param {string} password
- * @returns {Promise<boolean>}
- */
-contactSchema.methods.isPasswordMatch = async function (password) {
-  const contact = this;
-  return bcrypt.compare(password, contact.password);
-};
-
-contactSchema.pre('save', async function (next) {
-  const contact = this;
-  if (contact.isModified('password')) {
-    contact.password = await bcrypt.hash(contact.password, 8);
-  }
-  next();
-});
-
-/**
  * @typedef Contact
  */
 const Contact = mongoose.model('Contact', contactSchema);
