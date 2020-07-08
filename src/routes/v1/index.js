729da3e92dc05672f9c969728cf1fa55550962f2
Jethro Estrada <jethroestrada237@gmail.com>
2020-07-09 05:35:42 +0800

    Updated docs

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const authRoute = require('./auth.route');
-const userRoute = require('./user.route');
 const contactRoute = require('./contact.route');
 const docsRoute = require('./docs.route');
 
 const router = express.Router();
 
-router.use('/auth', authRoute);
-router.use('/users', userRoute);
 router.use('/contacts', contactRoute);
 router.use('/docs', docsRoute);
 
--- src/validations/index.js
+++ src/validations/index.js
@@ -1,3 +1,1 @@
-module.exports.authValidation = require('./auth.validation');
-module.exports.userValidation = require('./user.validation');
 module.exports.contactValidation = require('./contact.validation');
