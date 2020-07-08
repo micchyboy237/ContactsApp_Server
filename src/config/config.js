8b31e2057a28b7835d6cce0a6a982909d6507740
Jethro Estrada <jethroestrada237@gmail.com>
2020-07-09 05:03:21 +0800

    Updated env host and port

--- src/config/config.js
+++ src/config/config.js
@@ -28,6 +28,7 @@ if (error) {
 
 module.exports = {
   env: envVars.NODE_ENV,
+  host: envVars.HOST,
   port: envVars.PORT,
   mongoose: {
     url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
